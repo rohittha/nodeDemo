1c4f730f99269009b2c2d11e305adca7f6fa2992
rohittha <SKULLSHOXHMS@GMAIL.COM>
2022-09-16 11:49:45 -0400

    Mongoose demo

--- server.js
+++ server.js
@@ -3,30 +3,54 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-var messages = [
-    {name:'Tim', message : 'Hi from Tim'},
-    {name:'Hane', message : 'Hello from Jane'}
-]
+//var dbUrl = 'mongodb+srv://rohit:Qwerty1@cluster0.fvyaa.mongodb.net/?retryWrites=true&w=majority'
+var dbUrl = 'mongodb+srv://rohit:Qwerty1@cluster0.fvyaa.mongodb.net/learning-node'
+
+var Message = mongoose.model('Message', 
+    {
+        name: String,
+        message: String
+    }
+)
+
+// var messages = [
+//     {name:'Tim', message : 'Hi from Tim'},
+//     {name:'Hane', message : 'Hello from Jane'}
+// ]
 
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
 })
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body)
-    io.emit('messageEvent', req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+    message.save((err) => {
+        if (err)
+            sendStatus(500) 
+        
+        io.emit('messageEvent', req.body)
+        res.sendStatus(200)
+    })
+
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
 })
 
+mongoose.connect(dbUrl, (err) => {
+    if (err) console.log('mongo db connection', err);
+
+    console.log('mongo db connection', 'No-error!');
+})
 
 var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
